764cc6d96e11e73a78906bbabd85fd0ca13d88f2
theluanz <luanzuffo.lz@gmail.com>
2025-06-19 23:45:13 -0300

    Define e configura estrutura do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'vox-empresas';
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AlertModule.forRoot(),
+    ModalModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+  ],
+  exports: [CommonModule, AlertModule, ModalModule, ButtonsModule, CollapseModule, TooltipModule],
+})
+export class SharedModule {}
